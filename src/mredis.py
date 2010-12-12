624adc78d3983ab46624433a3c31c8f1952e1ea8
Gavin M. Roy <gmr@myyearbook.com>
2010-12-12 14:14:43 -0500

    Add some comment signature comments and pep8 cleanup

--- src/mredis.py
+++ src/mredis.py
@@ -23,7 +23,7 @@ class UnextendedRedisCommand(Exception):
 
 class MRedis:
 
-    def __init__(self, config, hash_method = 'standard'):
+    def __init__(self, config, hash_method='standard'):
         """
         Expects a list of dictionaries containing host, port, db:
 
@@ -59,15 +59,17 @@ class MRedis:
 
         raise InvalidHashMethod
 
-
     def get_server_key(self, server):
         "Return a string of server:port:db"
 
         return "%s:%i:%i" % (server.host, server.port, server.db)
 
-
     #### SERVER INFORMATION ####
     def bgrewriteaof(self):
+        """
+        Run the bgrewriteoaf command for each server returning success indexed
+        in a dictionary by server
+        """
 
         response = {}
         for server in self.servers:
@@ -76,6 +78,10 @@ class MRedis:
         return response
 
     def bgsave(self):
+        """
+        Run the bgsave command for each server returning success indexed in a
+        dictionary by server
+        """
 
         response = {}
         for server in self.servers:
@@ -83,8 +89,8 @@ class MRedis:
             response[key] = server.bgsave()
         return response
 
-
     def dbsize(self):
+        "Return the size of the database in a dictionary keyed by server"
 
         response = {}
         for server in self.servers:
@@ -92,8 +98,11 @@ class MRedis:
             response[key] = server.dbsize()
         return response
 
-
     def flushall(self):
+        """
+        Flushes all databases for each redis server returning success indexed
+        in a dictionary by server
+        """
 
         response = {}
         for server in self.servers:
@@ -101,8 +110,11 @@ class MRedis:
             response[key] = server.flushall()
         return response
 
-
     def flushdb(self):
+        """
+        Flushes the selected db for each redis server returning success indexed
+        in a dictionary by server
+        """
 
         response = {}
         for server in self.servers:
@@ -110,8 +122,8 @@ class MRedis:
             response[key] = server.flushdb()
         return response
 
-
     def info(self):
+        "Returns a dictionary keyed by Redis server of info command output"
 
         response = {}
         for server in self.servers:
@@ -119,17 +131,17 @@ class MRedis:
             response[key] = server.info()
         return response
 
-
     def lastsave(self):
+        "Returns a dictionary keyed by Redis server of lastsave command output"
 
         response = {}
         for server in self.servers:
             key = self.get_server_key(server)
             response[key] = server.lastsave()
         return response
-
 
     def ping(self):
+        "Returns a dictionary keyed by Redis server of ping command output"
 
         response = {}
         for server in self.servers:
@@ -137,8 +149,8 @@ class MRedis:
             response[key] = server.lastsave()
         return response
 
-
     def save(self):
+        "Returns a dictionary keyed by Redis server of save command output"
 
         response = {}
         for server in self.servers:
@@ -146,61 +158,51 @@ class MRedis:
             response[key] = server.save()
         return response
 
-
     ### Basic Key Commands
     def append(self, key, value):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].append(key, value)
 
-
     def delete(self, key):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].delete(key)
-
 
     def decr(self, key, amount=1):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].decr(key, amount)
 
-
     def exists(self, key):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].exists(key)
-
 
     def expire(self, key, time):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].exists(key, time)
 
-
     def expireat(self, key, when):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].exists(key, when)
-
 
     def get(self, key):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].get(key)
 
-
     def getset(self, key, value):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].getset(key, value)
 
-
     def incr(self, key, amount=1):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].incr(key, amount)
-
 
     def keys(self, pattern="*"):
 
@@ -210,23 +212,37 @@ class MRedis:
             response[key] = server.keys(pattern)
         return response
 
-
     def mget(self):
-        raise UnextendedRedisCommand
+        """
+        Currently unimplemented due to complexity of perserving this behavior
+        properly with multiple servers.
+        """
 
+        raise UnextendedRedisCommand
 
     def move(self):
+        """
+        Currently unimplemented due to complexity of perserving this behavior
+        properly with multiple servers.
+        """
 
         raise UnextendedRedisCommand
-
 
     def mset(self):
-        raise UnextendedRedisCommand
+        """
+        Currently unimplemented due to complexity of perserving this behavior
+        properly with multiple servers.
+        """
 
+        raise UnextendedRedisCommand
 
     def msetnx(self):
-        raise UnextendedRedisCommand
+        """
+        Currently unimplemented due to complexity of perserving this behavior
+        properly with multiple servers.
+        """
 
+        raise UnextendedRedisCommand
 
     def randomkey(self):
 
@@ -246,139 +262,116 @@ class MRedis:
         # @TODO Write this function, will have to manually perform functions
         raise UnextendedRedisCommand
 
-
     def set(self, key, value):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].set(key, value)
-
 
     def setex(self, key, value, time):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].setex(key, value, time)
 
-
     def substr(self, key, start, end=-1):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].substr(key, start, end)
-
 
     def ttl(self, key):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].ttl(key)
 
-
     def type(self, key):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].type(key)
-
 
     def watch(self, key):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].watch(key)
 
-
     def unwatch(self, key):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].unwatch(key)
 
-
     ### List Commands ###
     def blpop(self, keys, timeout=0):
-
         # @TODO Write this function, will have to manually perform functions
-        raise UnextendedRedisCommand
 
+        raise UnextendedRedisCommand
 
     def brpop(self, keys, timeout=0):
-
         # @TODO Write this function, will have to manually perform functions
-        raise UnextendedRedisCommand
 
+        raise UnextendedRedisCommand
 
     def lindex(self, key, index):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].lindex(key, index)
 
-
     def linsert(self, key, where, refvalue, value):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].linsert(key, where, refvalue, value)
 
-
-    def llen (self, key):
+    def llen(self, key):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].llen(key)
 
-
-    def lpop (self, key):
+    def lpop(self, key):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].lpop(key)
 
-
-    def lpush (self, key, value):
+    def lpush(self, key, value):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].lpush(key, value)
 
-
-    def lpushx (self, key, value):
+    def lpushx(self, key, value):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].lpushx(key, value)
 
-
-    def lrange (self, key, start, end):
+    def lrange(self, key, start, end):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].lrange(key, start, end)
-
 
     def lrem(self, key, value, num=0):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].lrem(key, value, num)
 
-
     def lset(self, key, index, value):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].set(key, index, value)
-
 
     def ltrim(self, key, start, end):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].ltrim(key, start, end)
 
-
-    def rpop (self, key):
+    def rpop(self, key):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].rpop(key)
 
-
-    def rpush (self, key, value):
+    def rpush(self, key, value):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].rpush(key, value)
 
-
-    def rpushx (self, key, value):
+    def rpushx(self, key, value):
 
         offset = self.get_node_offset(key)
         return self.servers[offset].rpushx(key, value)
-
 
     def sort(self, key, start=None, num=None, by=None, get=None,
              desc=False, alpha=False, store=None):
@@ -386,4 +379,3 @@ class MRedis:
         offset = self.get_node_offset(key)
         return self.servers[offset].store(key, start, num, by, get, desc,
                                           alpha, None)
-
